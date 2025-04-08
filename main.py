6e25853d0f3bf0cef3dbb68f97ca4236d8dcf3a1
Marte Solli Vågen <martvaag@stud.ntnu.no>
2025-04-08 19:11:08 +0200

    file to run the application

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from gui import ProteinStructurePredictorGUI
 # file thats runs the application
 if __name__ == "__main__":
     app = ProteinStructurePredictorGUI()
-    app.mainloop()
+    app.mainloop() 
